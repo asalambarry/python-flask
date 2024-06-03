fb7491a935aa210f1732754f0ddaa32be3686d2b
Barry <a.salambarry@gmail.com>
2024-06-04 00:22:57 +0200

    init project to python flask

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html", "./templates/**/*.twig", "./templates/**/*.php", "./templates/**/*.js", "./templates/**/*.vue", "./templates/**/*.jsx", "./templates/**/*.ts", "./templates/**/*.tsx", "./templates/**/*.md", "./templates/**/*.mdx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
